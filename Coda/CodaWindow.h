278459aa82358446d9935e9538f4c4fdaa69c33f
Tyler Robinson <tylerrhb11@gmail.com>
2022-10-13 13:28:48 -0400

    created CodaWindow singleton

--- Coda/CodaWindow.h
+++ Coda/CodaWindow.h
@@ -1,14 +1,20 @@
 #pragma once
 
+#include "pch.h"
 #include "CodaUtil.h"
 
 namespace Coda
 {
 	class CODA_API CodaWindow
 	{
-	private:
-
 	public:
-
+		void Init();
+		CodaWindow* GetWindow();
+		
+		void Create(int width, int height, const std::string& windowName);
+		void SwapBuffers();
+	private:
+		CodaWindow();
+		inline static CodaWindow* instance{ nullptr };
 	};
 }
