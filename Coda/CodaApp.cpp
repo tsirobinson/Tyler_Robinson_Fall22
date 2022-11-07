e0e63fff2bc31b9acaf51deeec9eb82425c8b1a9
Tyler Robinson <tylerrhb11@gmail.com>
2022-11-07 14:15:25 -0500

    adding textures and stbi code

--- Coda/CodaApp.cpp
+++ Coda/CodaApp.cpp
@@ -31,14 +31,18 @@ namespace Coda {
 			1, 2, 3 //second triangle (bottom right, top left, top right)
 		};
 		
-		unsigned int VBO, VAO;
+		unsigned int VBO, VAO, EBO;
     		glGenVertexArrays(1, &VAO);
     		glGenBuffers(1, &VBO);
+		glGenBuffers(1, &EBO);
 		
 		glBindVertexArray(VAO);
 		
 		glBindBuffer(GL_ARRAY_BUFFER, VBO);
    		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+		
+		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+		glBufferData(GL_ELEMENT_BUFFER, sizeof(indices), GL_STATIC_DRAW);
 		
 		//Describe attribute pointer 0 (positions)
     		glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)0);
@@ -96,6 +100,8 @@ namespace Coda {
 	    	}
 	    	glDeleteShader(vertexShader);
 	    	glDeleteShader(fragmentShader);
+		
+		////////// TEXTURES //////////
 
 		while (true) {
 			processInput(window);
